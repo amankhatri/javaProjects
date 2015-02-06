549c1a23c6b030ca09a9f55cce5d6a49b545bc31
amankhatri <aman_khatri5b5@yahoo.com>
2015-02-05 19:29:35 -0600

    intro

---
+++ D1P1 HelloJava/src/HelloWorld.java
@@ -0,0 +1,11 @@
+/*javac is java compiler and java.exe is java interprator*/
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*Main acts as a starting point for the application*/
+		/*All classes start from big letters*/
+		System.out.println("Hello World");
+	}
+
+}
