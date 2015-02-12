6c6d7f6b5acb7c9b34ec247777903e67458f7a34
amankhatri <aman_khatri5b5@yahoo.com>
2015-02-11 19:55:55 -0600

    Strings
    /*1)string is an immutable object, once it gets assigned I can not
    change it,
    *2) what java does is, it creates a memory for a string lets, say
    String1 = "Memory1"
    *3) and if I do String1 = "Memory2" then it just doesn;t over writes
    memory1 instead it creates
    *a new memory space*/
    public class Application {
    
    public static void main(String[] args) {
    // TODO Auto-generated method stub
    String name = new String("java");
    /*look up apis*/
    }
    
    }

---
+++ D7P2/src/Application.java
@@ -0,0 +1,13 @@
+/*1)string is an immutable object, once it gets assigned I can not change it, 
+ *2) what java does is, it creates a memory for a string lets, say String1 = "Memory1"
+ *3) and if I do String1 = "Memory2" then it just doesn;t over writes memory1 instead it creates
+ *a new memory space*/
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = new String("java");
+		/*look up apis*/
+	}
+
+}
