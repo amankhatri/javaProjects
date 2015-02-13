ceb5dbeb8a3986fefd0ba8c57d10df71f21844b2
amankhatri <aman_khatri5b5@yahoo.com>
2015-02-12 20:04:40 -0600

    Exception Handling
    /*
    * try {
    * code which can cause problem
    * }
    * catch(Exception objectofExcepetionClass) {
    * throw some error
    * }
    * when an exception or a problem occurs java will create an object of
    Exception class
    * Exception class will create an object, which will be caught by
    exception object.
    *
    *
    * */

---
+++ D8P2/src/Application.java
@@ -0,0 +1,40 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+/*
+ * try {
+ * code which can cause problem
+ * }
+ * catch(Exception objectofExcepetionClass) {
+ * throw some error
+ * }
+ * when an exception or a problem occurs java will create an object of Exception class
+ * Exception class will create an object, which will be caught by exception object.
+ * 
+ * 
+ * */
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner(System.in);
+	
+			try {
+				System.out.println("Please enter an Integer value");
+				int a = scan.nextInt();
+				System.out.println("Please enter another Integer Value");
+				int b = scan.nextInt();
+				System.out.println("Sum = " + a+b);
+				
+			}catch(InputMismatchException ob){
+				System.out.println("Value you entered is not valid please try again");
+			}catch(NoSuchElementException ob){
+				System.out.println("Error Occured");
+			}
+		
+		
+		System.out.println("End of program");
+	}
+
+}
