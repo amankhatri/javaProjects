360b63414d67b9a3f1bfe5c7d6ce0e368e78bddc
amankhatri <aman_khatri5b5@yahoo.com>
2015-02-25 20:04:30 -0600

    JDBC theory

---
+++ D15P1/src/DataBaseTheory.java
@@ -0,0 +1,24 @@
+/*database gives you an ability to store data in form of tables which contains columns and rows. 
+ * rows are called the records and the columns are called columns in database technology. While installing 
+ * the mysql workbench please remember 
+ * 1) the port number use default, do not change and also note it down --3306
+ * 2) keep username root if you change it note it -root
+ * 3) password - please note down password 
+ * 
+ * Steps 
+ * 1) make a new connection 
+ * 2) after creadng new connection, double click it
+ * 3) create schemas before creating tables 
+ * 		it is a container where we can create tables , we can create as many tables as we
+ * 		want in schemas
+ * 
+ * 
+ * Since java can not talk to db directly, there was a new set of class developed called JDBC, that 
+ * stand for JAva DataBase Connectivity. 
+ * */
+
+public class DataBaseTheory {
+	
+	/*JDBC, data base connectivity*/
+
+}
